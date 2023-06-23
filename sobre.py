c10cdff4eb33c9643bec79830712d7c56289247d
Gustavo Adolpho Bonesso <gustavo_bonesso@hotmail.com>
2023-06-23 10:31:16 -0300

    Tela Sobre

--- sobre.py
+++ sobre.py
@@ -25,12 +25,21 @@ ScrollView:
             center_x: self.center_x
 
         Label:
+            markup: True
             size_hint_y: None
             height: self.texture_size[1]
             text_size: self.width, None
             padding: 10, 10
             text:
-                'Texto teste texto teste sobre...\\n' * 20
+                "O Blink é um aplicativo para dispositivos móveis que monitora a condição de pacientes que sofrem" +\
+                "de blefaroespasmo através da avaliação da frequência das piscadas do paciente. \\n\\n" +\
+                "Ele foi desenvolvido para a disciplina Desenvolvimento de Aplicativos para Inovação Tecnológica " +\
+                "do programa de Mestrado Profissional em Inovação Tecnológica do Instituto de Ciência e Tecnologia " +\
+                "da Universidade Federal de São Paulo - Campus São José dos Campos. \\n\\n" +\
+                "A responsável pela disciplina é a [b]Regina Celia Coelho[/b] e a mentoria foi feita pelo " +\
+                "[b]Guilherme Melo Dos Santos[/b]. \\n\\n" +\
+                "O código é público e está disponível em [i]https://github.com/gbonesso/blink-app[/i].\\n\\n" +\
+                "O autor pode ser contatado pelo email [i]gustavo_bonesso@hotmail.com[/i]."
 
         Image:
             source: 'assets/Unifesp_simples_verde_negativo_RGB.png'
